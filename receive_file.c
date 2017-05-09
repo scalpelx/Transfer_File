3736e47f0bfad423d80df8e4620e54fd8a473e60
scalpelx <xieyongpeng@outlook.com>
2017-05-09 21:31:25 +0800

    add code comments

--- receive_file.c
+++ receive_file.c
@@ -11,8 +11,8 @@
 
 void writefile(int sockfd, FILE *fp) 
 {
-    ssize_t n;
-    char buff[MAX_LINE] = {0};
+    ssize_t n; //每次接受数据数量
+    char buff[MAX_LINE] = {0}; //数据缓存
     while ((n = recv(sockfd, buff, MAX_LINE, 0)) > 0) 
     {
         if (n == -1)
@@ -20,70 +20,80 @@ void writefile(int sockfd, FILE *fp)
             perror("Receive File Error");
             exit(1);
         }
+        
+        //将接受的数据写入文件
         if (fwrite(buff, sizeof(char), n, fp) != n)
         {
             perror("Write File Error");
             exit(1);
         }
-        memset(buff, 0, MAX_LINE);
+        memset(buff, 0, MAX_LINE); //清空缓存
     }
 }
 
 int main(int argc, char *argv[]) 
 {
-
+    //创建TCP套接字
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) 
     {
         perror("Can't allocate sockfd");
         exit(1);
     }
-
+    
+    //配置服务器套接字地址
     struct sockaddr_in clientaddr, serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(SERVERPORT);
 
+    //绑定套接字与地址
     if (bind(sockfd, (const struct sockaddr *) &serveraddr, sizeof(serveraddr)) == -1) 
     {
         perror("Bind Error");
         exit(1);
     }
 
+    //转换为监听套接字
     if (listen(sockfd, LINSTENPORT) == -1) 
     {
         perror("Listen Error");
         exit(1);
     }
 
+    //等待连接完成
     socklen_t addrlen = sizeof(clientaddr);
-    int connfd = accept(sockfd, (struct sockaddr *) &clientaddr, &addrlen);
+    int connfd = accept(sockfd, (struct sockaddr *) &clientaddr, &addrlen); //已连接套接字
     if (connfd == -1) 
     {
         perror("Connect Error");
         exit(1);
     }
-    close(sockfd);
+    close(sockfd); //关闭监听套接字
 
-    char filename[BUFFSIZE] = {0};
+    //获取文件名
+    char filename[BUFFSIZE] = {0}; //文件名
     if (recv(connfd, filename, BUFFSIZE, 0) == -1) 
     {
         perror("Can't receive filename");
         exit(1);
     }
 
+    //创建文件
     FILE *fp = fopen(filename, "wb");
     if (fp == NULL) 
     {
         perror("Can't open file");
         exit(1);
     }
-
+    
+    //把数据写入文件
     printf("Start receive file: %s from %s\n", filename, inet_ntoa(clientaddr.sin_addr));
     writefile(connfd, fp);
     puts("Receive Success");
 
+    //关闭文件和已连接套接字
     fclose(fp);
     close(connfd);
     return 0;
--- send_file.c
+++ send_file.c
@@ -9,34 +9,38 @@
 #include <sys/socket.h>
 #include "transfer.h"
 
-void readfile(FILE *fp, int sockfd) 
+void sendfile(FILE *fp, int sockfd) 
 {
-    int n;
-    char sendline[MAX_LINE] = {0};
+    int n; //每次读取数据数量
+    char sendline[MAX_LINE] = {0}; //暂存每次读取的数据
     while ((n = fread(sendline, sizeof(char), MAX_LINE, fp)) > 0) 
     {
-        if (n != MAX_LINE && ferror(fp))
+        if (n != MAX_LINE && ferror(fp)) //读取出错并且没有到达文件结尾
         {
             perror("Read File Error");
             exit(1);
         }
+        
+        //将读取的数据发送到TCP发送缓冲区
         if (send(sockfd, sendline, n, 0) == -1)
         {
             perror("Can't send file");
             exit(1);
         }
-        memset(sendline, 0, MAX_LINE);
+        memset(sendline, 0, MAX_LINE); //清空暂存字符串
     }
 }
 
 int main(int argc, char* argv[])
 {
+    //判断参数
     if (argc != 3) 
     {
         perror("usage:send_file filepath <IPaddress>");
         exit(1);
     }
 
+    //创建TCP套接字
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
     {
@@ -44,35 +48,35 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
+    //设置传输对端套接字地址
     struct sockaddr_in serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(SERVERPORT);
-    if (inet_pton(AF_INET, argv[2], &serveraddr.sin_addr) < 0)
+    if (inet_pton(AF_INET, argv[2], &serveraddr.sin_addr) < 0) //将IP地址格式从字符串转换为二进制
     {
         perror("IPaddress Convert Error");
         exit(1);
     }
 
+    //建立连接
     if (connect(sockfd, (const struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
     {
         perror("Connect Error");
         exit(1);
     }
-
-    FILE *fp = fopen(argv[1], "rb");
-    if (fp == NULL) 
-    {
-        perror("Can't open file");
-        exit(1);
-    }
-
-    char *filename = basename(argv[1]);
+    
+    //获取文件名
+    char *filename = basename(argv[1]); //文件名
     if (filename == NULL)
     {
         perror("Can't get filename");
         exit(1);
     }
+    
+    /*发送文件名
+      为了将文件名一次发送出去，而不是暂存到TCP发送缓冲区中，避免对方收到多余的数据，不好解析正确的文件名，
+      需要将要发送的数据大小设置为缓冲区大小*/
     char buff[BUFFSIZE] = {0};
     strncpy(buff, filename, strlen(filename));
     if (send(sockfd, buff, BUFFSIZE, 0) == -1)
@@ -80,9 +84,20 @@ int main(int argc, char* argv[])
         perror("Can't send filename");
         exit(1);
     }
+    
+    //打开要发送的文件
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL) 
+    {
+        perror("Can't open file");
+        exit(1);
+    }
 
-    readfile(fp, sockfd);
+    //读取并发送文件
+    sendfile(fp, sockfd);
     puts("Send Success");
+    
+    //关闭文件和套接字
     fclose(fp);
     close(sockfd);
     return 0;
