9738280a3027647b956532ae1151b81d7d68d848
scalpelx <xieyongpeng@outlook.com>
2017-05-09 21:34:31 +0800

    add code comments

--- transfer.h
+++ transfer.h
@@ -1,9 +1,13 @@
 #ifndef TRANSFER_FILE_TRANSFER_H
 #define TRANSFER_FILE_TRANSFER_H
 
+//读写数据大小
 #define MAX_LINE 4096
+//监听端口
 #define LINSTENPORT 7788
+//服务器端口
 #define SERVERPORT 8877
+//缓存大小
 #define BUFFSIZE 4096
 
 #endif //TRANSFER_FILE_TRANSFER_H
